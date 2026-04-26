4a54afaf976f1b25f293747dc8f09f4d596cd9c2
mottameister <murilo.m.fernandes@hotmail.com>
2026-04-26 17:02:01 -0400

    fix webpack compat

--- docs/intro.md
+++ docs/intro.md
@@ -1,15 +1,4 @@
 ---
 sidebar_position: 1
+slug: /
 ---
-
-# Owl's Nest Wiki
-
-Technical documentation for **A Toca da Coruja** campaign.
-
-Built on top of Radical Cobblemon Trainers.
-
-## Core Features
-
-- Structured progression
-- Competitive battles
-- Custom trainers
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,132 +1,108 @@
 // @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
 import {themes as prismThemes} from 'prism-react-renderer';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-/** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Owl's Nest Wiki",
   tagline: "Radical Cobblemon Trainers Campaign",
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: 'https://wiki.mottameister.xyz',
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mottameister',
+  projectName: 'owls-nest-wiki',
 
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
   },
 
   presets: [
-  [
-    'classic',
-    {
-      docs: {
-        sidebarPath: './sidebars.js',
-        routeBasePath: '/',
-        homePageId: 'intro',
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.js',
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
       },
-      blog: false,
-      theme: {
-        customCss: './src/css/custom.css',
-      },
-    },
+    ],
   ],
-],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      colorMode: {
-        respectPrefersColorScheme: true,
-      },
-      navbar: {
-  title: "Owl's Nest Wiki",
-  items: [
-    {
-      type: 'docSidebar',
-      sidebarId: 'tutorialSidebar',
-      position: 'left',
-      label: 'Docs',
+  themeConfig: {
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
     },
-    {
-      href: 'https://github.com/mottameister/owls-nest-wiki',
-      label: 'GitHub',
-      position: 'right',
+    navbar: {
+      title: "Owl's Nest Wiki",
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/mottameister/owls-nest-wiki',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
     },
-  ],
-},
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro/blog/',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.com/invite/TcSFAXGr6a',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/mottameister',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/docs/intro/blog/',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/mottameister/owls-nest-wiki',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: "Owl's Nest Wiki",
+              to: '/',
+            },
+            {
+              label: 'Download',
+              to: '/download',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.com/invite/TcSFAXGr6a',
+            },
+            {
+              label: 'X',
+              href: 'https://x.com/mottameister',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/mottameister/owls-nest-wiki',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} MottaMeister. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
 };
 
 export default config;
